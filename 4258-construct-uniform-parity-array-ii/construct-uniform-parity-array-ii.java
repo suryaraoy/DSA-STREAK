faa76abd271fb8b7959a67a1a7e1f1857ad7f6ff
Suryarao Yedla <suryaraoyedla3@gmail.com>
2026-09-04 03:20:33 +0530

    Time: 2 ms (100.00%) | Memory: 121.6 MB (54.55%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -5,11 +5,17 @@ class Solution {
         for(int i=1;i<n;i++){
             if(nums1[i]<min) min=nums1[i];
         }
+        // convert all elements is odd
+        //check if smallest element is odd then we can make every element in nums2 is odd
         if(min%2==1) return true;
-
+        
+        //convert all elements is even if possible
+        //if one element is odd we can't convert every element even in nums2
         for(int nums2 : nums1){
             if(nums2 % 2 ==1) return false;
         }
+
+        //then else condition so all elements present is nums1 is even 
         return true;
 
     }
