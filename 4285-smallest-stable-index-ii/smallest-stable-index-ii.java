8a07e07da1e70c41e213512cbedb7dcd04c92f0a
Suryarao Yedla <suryaraoyedla3@gmail.com>
2026-09-05 21:28:04 +0530

    Time: 3 ms (100.00%) | Memory: 133.1 MB (32.44%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -4,12 +4,12 @@ class Solution {
         int minElement=Integer.MAX_VALUE;
         int[] minElementIndex=new int[n];
         for(int i=n-1;i>=0;i--){
-            minElement=Math.min(minElement,nums[i]);
+           if(nums[i] < minElement) minElement=nums[i];
             minElementIndex[i]=minElement;
         }
         int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-                max=Math.max(max,nums[i]);
+                if(nums[i] > max) max=nums[i];
                 if(max-minElementIndex[i]<=k) return i;
         }
         return -1;
