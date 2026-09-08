91bd80c6723cb057f4851eca22b2c6b9942eb69b
Suryarao Yedla <suryaraoyedla3@gmail.com>
2026-09-09 00:14:13 +0530

    Time: 6 ms (11.53%) | Memory: 42.5 MB (72.85%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        int count=0;
+        for(int i=1000;i<=n;i++){
+            count++;
+        }
+        if(count!=0) return count;
+        return count;
+    }
+}
